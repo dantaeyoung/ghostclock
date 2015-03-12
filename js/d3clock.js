87ce7e53383c63b7191c15ae786a8f00e365623e
Dan Taeyoung <provolot@gmail.com>
2015-03-12 01:07:04 -0400

    using flexbox

--- js/d3clock.js
+++ js/d3clock.js
@@ -1,40 +1,58 @@
-function initGeolocation()
- {
-	if( navigator.geolocation ) {
-		// Call getCurrentPosition with success and failure callbacks
-		navigator.geolocation.getCurrentPosition( success, fail );
+var getUrlValue = function(VarSearch){
+	var SearchString = window.location.search.substring(1);
+	var VariableArray = SearchString.split('&');
+	for(var i = 0; i < VariableArray.length; i++){
+		var KeyValuePair = VariableArray[i].split('=');
+		if(KeyValuePair[0] === VarSearch){
+			return KeyValuePair[1];
+		}
 	}
-	else {
-		alert("Sorry, your browser does not support geolocation services.");
-	}
- }
+}
 
- function success(position)
- {
+
+function initGeolocation() {
+	if(!(getUrlValue('destination') === undefined)) {
+		window.destination = getUrlValue('destination');
+		if( navigator.geolocation ) {
+			// Call getCurrentPosition with success and failure callbacks
+			navigator.geolocation.getCurrentPosition( success, fail );
+		}
+		else {
+			alert("Sorry, your browser does not support geolocation services.");
+		}
+	} else {
+		alert("Sorry, no location entered!");
+	}
+}
+
+function success(position) {
 	getTransitTime(position.coords.latitude, position.coords.longitude);
- }
+}
 
- function fail()
- {
+function fail() {
 	// Could not obtain location
- }
-
+}
 
 function getTransitTime(lat, lon) {
-	window.destAddress='40.696319,-73.960623'
-
 	var directionsService = new google.maps.DirectionsService();
-     var request = {
-       origin: '40.7551582,-73.9410288999999',
-       destination: window.destAddress,
-       travelMode: google.maps.DirectionsTravelMode.TRANSIT
-     };
-     directionsService.route(request, function(response, status) {
-       if (status == google.maps.DirectionsStatus.OK) {
+	var request = {
+		origin: lat + "," + lon,
+		destination: '40.696319,-73.960623',
+		travelMode: google.maps.DirectionsTravelMode.TRANSIT
+	};
+	directionsService.route(request, function(response, status) {
+		console.log (lat + "," + lon);
+
+		if (status == google.maps.DirectionsStatus.OK) {
 			console.log(response);
-var arrival_time = response.routes[0].legs[0].arrival_time.value;
-			window.transitHandData[0].value = arrival_time.getHours() % 12 + (arrival_time.getMinutes() / 60);
-			window.transitHandData[1].value = arrival_time.getMinutes();
+			if(arrival_time in response.routes[0].legs[0]) {
+				var arrival_time = response.routes[0].legs[0].arrival_time.value;
+			} else {
+				var arrival_time = new Date();
+				arrival_time.setSeconds(arrival_time.getSeconds() + response.routes[0].legs[0].duration.value); 
+			}
+			window.transitHandData[0].value = arrival_time.getHours() % 12 + (arrival_time.getMinutes() / 60.0);
+			window.transitHandData[1].value = arrival_time.getMinutes() + (arrival_time.getSeconds() / 60.0);
 			window.transitHandData[2].value = arrival_time.getSeconds();
 			console.log(window.transitHandData);
 /*			$("#arrival_time").html(response.routes[0].legs[0].arrival_time.text);
@@ -43,47 +61,27 @@ var arrival_time = response.routes[0].legs[0].arrival_time.value;
 				$("<div>").html(d.instructions).appendTo("#steps");
 			}); */
 
-
 			var transitHands = d3.select("#transit-hands");
 
 			transitHands.selectAll('path')
 				.data(window.transitHandData)
-					.enter()
-					.append('path')
-					.attr('class', function(d){
-						return d.type + '-transitHand transitHand';
-					})
-					.attr('d', function(d) {
-						return "M" + (d.width / 2.0) + " " + d.balance + " L" + (d.width / -2.0) + " " + d.balance + " L0 " + d.length + " Z"
-					})
-					.attr('transform',function(d){
-						return 'rotate(' + ((d.scale(d.value) + 30) % 360) + ')';
-					});
-
-
-       }
-     });
+				.enter()
+				.append('path')
+				.attr('class', function(d){
+					return d.type + '-transitHand transitHand';
+				})
+				.attr('d', function(d) {
+					return "M" + (d.width / 2.0) + " " + d.balance + " L" + (d.width / -2.0) + " " + d.balance + " L0 " + d.length + " Z"
+				})
+				.attr('transform',function(d){
+					return 'rotate(' + ((d.scale(d.value)) % 360) + ')';
+				});
+		}
+	});
 }
-
-
-
-
 
 var d3clockfunc = function(scope, elem, attrs) {
 
-
-	window.onresize = function(event) {
-		onWindowResize();
-	}
-
-	var onWindowResize = function() {
-		var svg = elem.find("svg")[0];
-		var targetDim = Math.min(window.innerWidth, window.innerHeight);
-		svg.setAttribute('height', targetDim);
-		svg.setAttribute('width', targetDim);
-		svg.style.marginTop = targetDim / -2.0;
-		console.log(svg);
-	};
 
 	var radians = 0.0174532925, 
 		clockRadius = 200,
@@ -193,7 +191,7 @@ var d3clockfunc = function(scope, elem, attrs) {
 		}
 	];
 
-	function drawClock(){ //create all the clock elements
+	function drawClock() { //create all the clock elements
 
 		updateData();	//draw them in the correct starting position
 		var rawSvg = elem.find("svg")[0];
@@ -224,7 +222,7 @@ var d3clockfunc = function(scope, elem, attrs) {
 		//
 		//... and hours
 		face.selectAll('.hour-tick')
-			.data(d3.range(0,11)).enter()
+			.data(d3.range(0,12)).enter()
 				.append('line')
 				.attr('class', 'hour-tick')
 				.attr('x1',0)
@@ -242,16 +240,14 @@ var d3clockfunc = function(scope, elem, attrs) {
 				.attr('class', 'hour-label')
 				.attr('text-anchor','middle')
 				.attr('x',function(d){
-					return hourLabelRadius*Math.sin(hourScale(d)*radians + Math.PI);
+					return hourLabelRadius*Math.sin(hourScale(d) * radians);
 				})
 				.attr('y',function(d){
-					return -hourLabelRadius*Math.cos(hourScale(d)*radians + Math.PI) + hourLabelYOffset;
+					return -hourLabelRadius*Math.cos(hourScale(d) * radians) + hourLabelYOffset;
 				})
 				.text(function(d){
-					return (d + 6) % 12 ;
+					return (d + 11) % 12 + 1;
 				});
-
-
 
 		face.append('g').attr('id','face-overlay')
 			.append('circle').attr('class','hands-cover')
@@ -280,7 +276,7 @@ var d3clockfunc = function(scope, elem, attrs) {
 
 	function moveHands(){
 		d3.select('#clock-hands').selectAll('path')
-		.data(handData)
+			.data(handData)
 			.transition()
 			.ease("linear")
 			.attr('transform',function(d){
@@ -293,48 +289,19 @@ var d3clockfunc = function(scope, elem, attrs) {
 		var elapsedSeconds = (3600 * t.getHours()) + (60 * t.getMinutes()) + t.getSeconds();
 		var elapsedMilliseconds = (elapsedSeconds * 1000) + t.getMilliseconds();
 
-		/* 
-		in one day, there are 11 undechours 
-		there are 11 
-		The hour hand should turn 360 degrees in a day; the minute hand should do 11 rotations of 360 degrees in a day. The second hand should do 1331 360 degree rotations in a day.
-		
-		24hour  =  secondselapsed  / 3600 -- secs in an hour (domain: 0~23, range: 0~360)
-		min = secondselapsed % 3600  -- secs in an hour (domain: 0~3599, range: 0~360)
-		sec = milliseconds elapsed % 60,000 -- millisecs in a minute (domain: 0~59999, range: 0~360)
-		hour = secselapsed (domain: 0~86399, range: 0~360)
-		minute = secondselapsed / 7854.545 (domain: 0~10, range: 0~360)
-		second = milliseconds elapsed % 66156.2021 -- (domain: 0~66155, range: 0~360)
-
-		handData[0].value = elapsedSeconds / 86400 * 11;
-		handData[1].value = elapsedSeconds / 7854.5455;
-		handData[2].value = elapsedMilliseconds % 66156.2021;
-		*/
-
-		handData[0].value = t.getHours() % 12;
-		handData[1].value = t.getMinutes();
+		handData[0].value = (t.getHours() % 12) + (t.getMinutes() / 60.0);
+		handData[1].value = t.getMinutes() + (t.getSeconds() / 60.0);
 		handData[2].value = t.getSeconds();
-
-		/*console.log(t.getHours());
-		console.log(t.getMinutes());
-		console.log(t.getSeconds()); */
-
-/*		console.log(elapsedSeconds / 86400.0 * 11.0);
-		console.log(elapsedSeconds % 7854.5455);
-		console.log( Math.round(elapsedMilliseconds % 66156.2021) + 273.375); */
 
 	}
 
 	drawClock();
 	initGeolocation();
 
-	onWindowResize();
-
 	setInterval(function(){
-//		console.log(handData);
 		updateData();
 		moveHands();
 	}, 100);
-	//}, 546.75);
 
 	d3.select(self.frameElement).style("height", height + "px");
 }
